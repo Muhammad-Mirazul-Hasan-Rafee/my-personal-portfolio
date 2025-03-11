c932ec609d0100fa684329948e7c83e412a2cf41
Rafee <rafeehasan88@gmail.com>
2025-03-11 14:10:53 +0600

    canvas added for model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,39 @@
-import {motion} from "framer-motion";
-import {styles} from "../styles";
-import {ComputersCanvas} from "./canvas"; 
-
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
-    <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
 
-      <div className="flex flex-col justify-center items-center mt-5">
-        {/* Vertical circle and line */}
-        {/* Vertical circle */}
-        <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-        {/* Vertical line */}
-        <div className="w-1 sm:h-80 h-40 violet-gradient" />
+      {/*---------------------Vertical circle and line || Text , Subtext----------- */}
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+
+        <div className="flex flex-col justify-center items-center mt-5">
+
+
+          {/* Vertical circle */}
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+
+
+          {/* Vertical line */}
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+
+
+        </div>
+
+        {/*----------------- for Text , subtext----------------- */}
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-[#915eff]">Rafee</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop frontend using <br className="sm:block hidden"/> Three Js and React</p>
+        </div>
       </div>
 
-      {/* for text */}
-      <div>
-        <h1>Hi, I'm <span className="text-[#915eff]">Rafee</span></h1>
-      </div>
+      <ComputersCanvas/>
+    </section>
+  );
+};
 
-    </div>
-  </section>
-  )
-}
-
-export default Hero
+export default Hero;
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React , { Suspense , useState , useEffect } from "react";
+import { Canvas , extend } from "@react-three/fiber";
+import { OrbitControls , Preload , useGLTF , Html } from "@react-three/drei";
+import CanvasLoader from "../Loader.jsx";
 
 const Computers = () => {
+  const computer = useGLTF("/public/desktop_pc/scene.gltf");
+  console.log(computer);
   return (
-    <div>Computers</div>
-  )
-}
+    // three js create create er jonno mesh tag use korte hbe
 
-export default Computers
+    <mesh>
+      {/* light create korte hobe, jeta chara kichu dekha jabe na */}
+    
+      <hemisphereLight intensity={0.15} groundColor='black' />
+
+      {/* point light create korte hbe -- eta pc er screen erupor dekha jabe just like room er light er reflection ta pc te porche */}
+      <pointLight intensity={1} />
+
+      {/* object pass korar jnno primitive use korte hbe */}
+      <primitive object={computer.scene} />
+
+    </mesh>
+  )
+};
+
+// camera - kothay theke amra model ta dekhbo setar way ..fov(field of view) is how wide the object is || {preserveDrawingBuffer:true = to render the model properly
+
+
+const ComputersCanvas = () =>{
+  return(
+    <Canvas 
+    frameloop="demand" 
+    shadows
+     camera={{position:[20 , 3 , 5] , fov: 25 }}
+    gl={{preserveDrawingBuffer:true}}>
+
+      {/* Suspense is rom react  , eta loader pete help korbe jokhn model ta loading hote thakbe */}
+      <Suspense fallback={<CanvasLoader/>}>
+       
+
+       {/* OrbitControls model ta k left and right e move korte help korbe || enableZoom er value false hbe karon amra eta k zoom korbo na.. ||       maxPolarAngle and minPolarAngle -> eigulo use kora hocche jno model ta sob jayga the rotate na kora jay, infact specific angle or axis thei rotate kora jno jay etai target*/}
+
+      <OrbitControls enableZoom={false}
+      maxPolarAngle={Math.PI/2}
+      minPolarAngle={Math.PI/2}/>
+
+      <Computers/>
+
+      </Suspense>
+      <Preload all />
+
+    </Canvas>
+    
+  )
+
+};
+
+export default ComputersCanvas;
